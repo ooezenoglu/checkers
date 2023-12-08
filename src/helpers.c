000eae11628cf8942f5f39dc7c042184debaf9ab
Özge Özenoglu <oezge.oezenoglu@campus.lmu.de>
2023-12-08 12:09:59 +0100

    `parseCommandLineArgs()`: make `-p` optional; bugfixes

--- src/helpers.c
+++ src/helpers.c
@@ -1,6 +1,3 @@
-/* damit getopt() deklariert wird */
-#define _POSIX_C_SOURCE 2
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -12,34 +9,56 @@
 int parseCommandLineArgs(int argc, char *argv[], struct gameInfo *gameDataPointer) {
 
     int opt;
+    int gflag = 0;
+    int pflag = 0;
 
-    if (argc <= 1) {
+    if (argc < 3) {
         perror("Incorrect format. Please enter the data in the format -g <GAME-ID> -p <{1,2}>.");
         return -1;
     }
 
-    /* parse command line arguments */
-    while ((opt = getopt(argc, argv, "g:p:")) != -1) {
+    while ((opt = getopt(argc, argv, ":g:p::")) != -1) {
         switch (opt) {
-
+            
             case 'g':
+
+                if (optarg == NULL || strlen(optarg) != 13) {
+                    perror("Game ID must be exactly 13 characters long.");
+                    return -1;
+                }
+
                 memcpy(gameDataPointer -> gameID, optarg, strlen(optarg));
                 (gameDataPointer -> gameID)[strlen(optarg)] = '\0';
 
-                if(strlen(gameDataPointer -> gameID) != 13) {
-                    perror("Game-ID must be exactly 13 characters long.");
-                    return -1;
-                }
+                /* game ID is set correctly */
+                gflag = 1;
+
                 break;
 
             case 'p':
-                gameDataPointer -> desPlayerNumber = atoi(optarg);
 
-                if(gameDataPointer -> desPlayerNumber != 1 && gameDataPointer -> desPlayerNumber != 2) {
-                    perror("The player number must be 1 or 2.");
-                    return -1;
+                if (optind < argc && !stringCompare(argv[optind], "-g")) { /* value for -p is set */
+                
+                    gameDataPointer -> desPlayerNumber = atoi(argv[optind]);
+
+                    if (gameDataPointer -> desPlayerNumber != 1 && gameDataPointer -> desPlayerNumber != 2) {
+                        perror("If set, the player number must be 1 (opponent is the computer) or 2 (opponent is human).");
+                        return -1;
+                    }
+
+                    /* player number is set correctly */
+                    pflag = 1;
                 }
+
                 break;
+
+            case ':':  
+                perror("Option -g needs a value.");  
+                return -1;
+
+            case '?':  
+                perror("Unknown option(s)."); 
+                return -1;
 
             default:
                 perror("Incorrect format. Please enter the data in the format -g <GAME-ID> -p <{1,2}>.");
@@ -47,9 +66,19 @@ int parseCommandLineArgs(int argc, char *argv[], struct gameInfo *gameDataPointe
         }
     }
 
+    if(gflag == 0) {
+        perror("Game ID unknown.");
+        return -1;
+    }
+
+    /* default value if -p is not set */
+    if(pflag == 0) {
+        gameDataPointer -> desPlayerNumber = -1;
+    }
+
     /* debugging */
-    printf("Game-ID: %s\n", gameDataPointer -> gameID);
-    printf("Spielernummer: %i\n", gameDataPointer -> desPlayerNumber);  
+    printf("Game ID: %s\n", gameDataPointer -> gameID);
+    printf("Player number: %i\n", gameDataPointer -> desPlayerNumber);  
 
     return 0;  
 }
